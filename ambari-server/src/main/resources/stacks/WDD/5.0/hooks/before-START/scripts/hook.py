471677362c7babd5eccde67793c7565fb4f748b3
Vladimir Strigun <vladimir.strigun@wandisco.com>
2017-04-06 12:08:10 +0100

    Create ranger plugin symlinks for yarn resourcemanager
    Change-Id: Ie64f309dc7da9fe216bff944d5476ea78c593d5d

--- ambari-server/src/main/resources/stacks/WDD/5.0/hooks/before-START/scripts/hook.py
+++ ambari-server/src/main/resources/stacks/WDD/5.0/hooks/before-START/scripts/hook.py
@@ -33,6 +33,7 @@ def setup_hadoop_env():
     create_ranger_hbase_symlinks()
     create_ranger_hive_symlinks()
     create_ranger_storm_symlinks()
+    create_ranger_yarn_symlinks()
 
 
 def create_javahome_symlink():
@@ -79,6 +80,21 @@ def create_ranger_storm_symlinks():
             #create symlinks to plugin libs only in case storm nimbus installed on host
             if(os.path.isdir("/usr/wdd/current/storm-nimbus/lib")) :
               setup_ranger_plugin_jar_symblink(stack_version, 'storm', component_list=['storm-nimbus'])
+
+def create_ranger_yarn_symlinks():
+    import params
+    ranger_admin_hosts = default("/clusterHostInfo/ranger_admin_hosts", [])
+    has_ranger_admin = not len(ranger_admin_hosts) == 0
+    if has_ranger_admin:
+        config = Script.get_config()
+        is_supported_yarn_ranger = config['configurations']['yarn-env']['is_supported_yarn_ranger']
+        if params.has_resourcemanager and is_supported_yarn_ranger :
+            from resource_management.libraries.functions.setup_ranger_plugin_xml import setup_ranger_plugin_jar_symblink
+            stack_version = get_stack_version('hadoop-client')
+            #create symlinks to plugin libs only in case resourcemanager installed on host and ranger is enabled for yarn
+            if(os.path.isdir("/usr/wdd/current/hadoop-yarn-resourcemanager/lib") and not os.path.exists("/usr/wdd/current/hadoop-yarn-resourcemanager/lib/ranger-yarn-plugin-impl")) :
+              Logger.info("Create ranger symlinks for hadoop-yarn-resourcemanager")
+              setup_ranger_plugin_jar_symblink(stack_version, 'yarn', component_list=['hadoop-client'])
 
 
 def copy_container_executor_cfg():
